cd98c6c33f4d9b63383b56d76f51fc92a4cf4e42
Aaron George <ageor88@wgu.edu>
2021-10-10 21:22:19 -0400

    Update and rename Rpg.py to main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def rpg_char():
     # Defining the alignment and Class variable globally
     global alignment
     global classes
-    
+
     # The alignment choices will be completely random
     align_choice = random.choice(alignment)
 
@@ -37,15 +37,16 @@ def rpg_char():
 
     # Once the random alignment is picked, the program informs the user of said alignment
     print("\nAlignment choice: " + align_choice)
-    print("You are now of " + align_choice)
+    print("You are now " + align_choice)
 
     try:
         # If the user is of Law alignment, the program will give a brief summary of the chose
         if align_choice == "Law":
             print("\nYou are the divine messenger of fate, a righteous warrior of light")
 
-        # User is expected to input a name, then a welcome message with their name is printed, and stats are assign
+            # User is expected to input a name, then a welcome message with their name is printed, and stats are assign
             global name
+
             name = input("Pick a name for your character: ")
             print(f"\nHello {name}, lets customize yourself."
                   " Here are your pre-determined stats: ")
@@ -53,16 +54,13 @@ def rpg_char():
 
             # User needs to pick a class from the selection
             class_choice = str(input("\nPick a class for you character:"
-                                 " Warrior, Knight, Mage, Thief, Cleric, Pyromancer: "))
-            for i in classes:
-                if class_choice in i:
-                    print(f"{class_choice} is your decision, are you happy with the character you created?: (y/n)")
+                                     " Warrior, Knight, Mage, Thief, Cleric, Pyromancer: "))
 
-                    # if the user is satisfied with their decision they can either proceed or stop
-                    while input() == "y":
-                        print(f"\nLet your journey begin, young {class_choice} of {align_choice}: press Enter ")
-                    else:
-                        pass
+            if class_choice in classes:
+                    print(f"\nLet your journey begin, young {class_choice} of {align_choice}\n")
+            else:
+                print("Not a class in the list\n")
+                rpg_char()
 
         # If the user is of Neutral alignment, the program will give a brief summary of the chose
         if align_choice == "Neutral":
@@ -77,15 +75,12 @@ def rpg_char():
             # User is expected to choose a class for their character
             class_choice = str(input("\nPick a class for you character: "
                                      "Warrior, Knight, Mage, Thief, Cleric, Pyromancer: "))
-            for i in classes:
-                if class_choice in i:
-                    print(f"{class_choice} is your decision, are you happy with the character you created?: (y/n)")
 
-                    # if the user is satisfied with their decision they can either proceed or stop
-                    while input() == "y":
-                        print(f"\nLet your journey begin, young {class_choice} of {align_choice}: press Enter ")
-                    else:
-                        pass
+            if class_choice in classes:
+                print(f"\nLet your journey begin, young {class_choice} of {align_choice}\n")
+            else:
+                print("Not a class in the list\n")
+                rpg_char()
 
         # If the user is of Chaos alignment, the program will give a brief summary of the chose
         if align_choice == "Chaos":
@@ -100,28 +95,21 @@ def rpg_char():
             # User is expected to choose a class for their character
             class_choice = str(input("\nPick a class for you character: "
                                      "Warrior, Knight, Mage, Thief, Cleric, Pyromancer: "))
-            for i in classes:
-                if class_choice in i:
-                    print(f"{class_choice} is your decision, are you happy with the character you created?: (y/n)")
 
-                    # if the user is satisfied with their decision they can either proceed or stop
-                    while input() == "y":
-                        print(f"\nLet your journey begin, young {class_choice} of {align_choice}: Press Enter ")
-                    else:
-                        pass
+            if class_choice in classes:
+                print(f"\nLet your journey begin, young {class_choice} of {align_choice}\n")
+            else:
+                print("Not a class in the list\n")
+                rpg_char()
 
     # For any exceptions that may occur
     except ValueError:
         print("Oops, not a letter. try again ")
-        return rpg_char()
+        rpg_char()
     except Exception as e:
         print(e)
         print("That method was wrong, please try again")
-        return rpg_char()
-
-
-# Displays function
-rpg_char()
+        rpg_char()
 
 
 def char_weapon():
@@ -130,20 +118,14 @@ def char_weapon():
     weapon_choice = input("What weapon will you choose:\n" + ", ".join(weapons))
 
     # It will iterate through the weapons list and if true the user will obtain that weapon
-    for k in weapons:
-        if weapon_choice in weapons:
-            print(f" Use your {weapon_choice} in battle the upcoming enemies, ")
-            break
-        else:
-            print("\nThat is not a weapon in the list, nice try")
-            return char_weapon()
+    if weapon_choice in weapons:
+        print(f"\nUse your {weapon_choice} in battle the upcoming enemies, ")
+    else:
+        print("\nThat is not a weapon in the list, nice try")
+        char_weapon()
 
     # Will start your first battle
     print("\nNow it is time to fight your first enemy")
-
-
-# Displays function
-char_weapon()
 
 
 def battle():
@@ -151,17 +133,15 @@ def battle():
     global enemies
     global enemies_health
     global health
-    global enemy_actions
 
     # Randomizes the enemies, their health, and actions
     ran_enemies = random.choice(enemies)
     ran_health = random.choice(enemies_health)
-    list_actions = random.choice(enemy_actions)
 
     # Prints the name of the enemy the user will battle
     print("Your first enemy is a {}".format(ran_enemies))
 
-    # Prints the UI like look of yourself and the enemy
+    # Prints the UI look of yourself and the enemy
     print(f"Name: "
           f"  {name}   {ran_enemies}"
           f"\nHP:  {health}    {ran_health}")
@@ -170,8 +150,10 @@ def battle():
 
 # The process for the battle, the user takes a turn then the enemy does, etc
     while True:
+        global enemy_actions
+        list_actions = random.choice(enemy_actions)
 
-        # If the user attacks the enemy is damage for 5 hp, then a message is displayed, then next turn
+        # If the user attacks the enemy is damage for 5 hp, then a message is displayed, the next turn
         if actions == "attack":
             ran_health += -5
             print("The {} took massive damage".format(ran_enemies))
@@ -182,19 +164,11 @@ def battle():
                             "\nDefend   \nRun: ")
 
             # The enemy will take either of 3 actions against the user
-            if list_actions == "attack":
+            if list_actions == "attack" or list_actions == "spell":
                 health += -4
-                print(f"The {ran_enemies} attacked")
+                print(f"The {ran_enemies} used {list_actions}")
                 print(f"\nName: "
                       f"\n{name}     {ran_enemies}"
-                      f"\nHP:  {health}    {ran_health} ")
-                actions = input("\nAttack  \nSpell"
-                                "\nDefend   \nRun: ")
-            elif list_actions == "spell":
-                health += -5
-                print(f"The {ran_enemies} used his spell attack")
-                print(f"\nName: "
-                      f"\n{name}    {ran_enemies}"
                       f"\nHP:  {health}    {ran_health} ")
                 actions = input("\nAttack  \nSpell"
                                 "\nDefend   \nRun: ")
@@ -203,20 +177,11 @@ def battle():
             elif list_actions == "defend":
                 ran_health += -2
                 print(f"The {ran_enemies} defended")
-                continue
-
-            # If either player or enemy's health reaches 0, the game ends
-            if ran_health == 0:
-                print("You defeated the enemy congrats!")
-                pass
-            elif health == 0:
-                print("You were defeated!")
-                pass
-
-        # If the user runs from the battle, the game instantly ends
-        if actions == "run":
-            print("You escaped battle, I guess you won?")
-            break
+                print(f"\nName: "
+                      f"\n{name}     {ran_enemies}"
+                      f"\nHP:  {health}    {ran_health} ")
+                actions = input("\nAttack  \nSpell"
+                                "\nDefend   \nRun: ")
 
         # Same principle, spell attacks take 5 hp from enemy
         if actions == "spell":
@@ -228,66 +193,79 @@ def battle():
             actions = input("\nAttack  \nSpell"
                             "\nDefend   \nRun: ")
 
-            # Enemy attacks take 3 hp from user
-            if list_actions == "attack":
-                health += -3
-                print(f"The {ran_enemies} attacked")
-                print(f"\nName: "
-                      f"\n{name}     {ran_enemies}"
-                      f"\nHP:  {health}    {ran_health} ")
-                actions = input("\nAttack  \nSpell"
-                                "\nDefend   \nRun: ")
-
-            # Enemy spell attacks take 4 damage from user
-            elif list_actions == "spell":
+            # Enemy attacks take 4 hp from user
+            if list_actions == "attack" or list_actions == "spell":
                 health += -4
-                print(f"The {ran_enemies} used his spell attack")
+                print(f"The {ran_enemies} used {list_actions}")
                 print(f"\nName: "
                       f"\n{name}     {ran_enemies}"
                       f"\nHP:  {health}    {ran_health} ")
                 actions = input("\nAttack  \nSpell"
                                 "\nDefend   \nRun: ")
 
-        # If enemy defends on a turn, it continues and takes less damage
-        if list_actions == "defend":
-            if actions == "attack" or "spell":
+            # If the enemy defends the user attack's, the enemy takes less damage
+            elif list_actions == "defend":
                 ran_health += -2
                 print(f"The {ran_enemies} defended")
-                continue
-
-            # Once again if either of opponents health reaches 0, the game ends
-            if ran_health == 0:
-                print("You defeated the enemy congrats!")
-                break
-            elif health == 0:
-                print("You were defeated!")
-                pass
+                print(f"\nName: "
+                      f"\n{name}     {ran_enemies}"
+                      f"\nHP:  {health}    {ran_health} ")
+                actions = input("\nAttack  \nSpell"
+                                "\nDefend   \nRun: ")
 
         # If user defends on a turn, it continues and takes less damage
         if actions == "defend":
-            if list_actions == "attack":
+            ran_health += 0
+            print("You blocked the {} attack ".format(ran_enemies))
+            print(f"\nName: "
+                  f"\n{name}     {ran_enemies}"
+                  f"\nHP:  {health}    {ran_health} ")
+            actions = input("\nAttack  \nSpell"
+                            "\nDefend   \nRun: ")
+
+            if list_actions == "attack" or list_actions == "spell":
                 health += -2
-                print(f"The {ran_enemies} attacked")
-                print(f"\nName: "
-                      f"\n{name}     {ran_enemies}"
-                      f"\nHP:  {health}    {ran_health} ")
-                actions = input("\nAttack  \nSpell"
-                                "\nDefend   \nRun: ")
-            elif list_actions == "spell":
-                health += -2
-                print(f"The {ran_enemies} used his spell attack")
+                print(f"The {ran_enemies} used {list_actions}")
                 print(f"\nName: "
                       f"\n{name}     {ran_enemies}"
                       f"\nHP:  {health}    {ran_health} ")
                 actions = input("\nAttack  \nSpell"
                                 "\nDefend   \nRun: ")
 
-            # If both opponents defend no one takes damage
             elif list_actions == "defend":
                 ran_health += 0
-                print(f"The {ran_enemies} defended")
-                continue
+            print(f"The {ran_enemies} defended")
+            print(f"\nName: "
+                  f"\n{name}     {ran_enemies}"
+                  f"\nHP:  {health}    {ran_health} ")
+            actions = input("\nAttack  \nSpell"
+                            "\nDefend   \nRun: ")
+
+        # If both opponents defend no one takes damage
+        if list_actions and actions == "defend":
+            ran_health += 0
+            print(f"You and the {ran_enemies} both defended")
+            print(f"\nName: "
+                  f"\n{name}     {ran_enemies}"
+                  f"\nHP:  {health}    {ran_health} ")
+            actions = input("\nAttack  \nSpell"
+                            "\nDefend   \nRun: ")
+
+        # If the user runs from the battle, the game instantly ends
+        if actions == "run":
+            print("You escaped battle, I guess you won?")
+            break
+
+        # If either player or enemy's health reaches 0, the game ends
+        if ran_health <= 0:
+            print("You defeated the enemy congrats!")
+            break
+        elif health <= 0:
+            print("You were defeated!")
+            break
 
 
-# Displays function
-battle()
+if __name__ == "__main__":
+    rpg_char()
+    char_weapon()
+    battle()
